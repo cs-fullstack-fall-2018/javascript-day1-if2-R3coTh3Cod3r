80d6c2e6d942ba02dda3248b8f7f57e1c09ccc48
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-31 09:57:07 -0500

    fix if2

--- ex11.js
+++ ex11.js
@@ -1,22 +1,22 @@
-var lettergrade=parseInt(prompt("Enter number "));
+var lettergrade=parseInt(prompt("Enter grade number "));
 
 if (lettergrade>101){
     alert("Error")
 }
-else if (lettergrade>90){
+else if (lettergrade>=90){
     alert("A")
 }
-else if (lettergrade>80){
+else if (lettergrade>=80){
     alert("B")
 }
-else if (lettergrade>70) {
+else if (lettergrade>=70) {
     alert("C")
 }
-else if (lettergrade>60){
+else if (lettergrade>=60){
     alert("D")
 
 } 
-else  if (lettergrade>59){
+else  if (lettergrade<60){
         alert("F")
     }
     else {
--- ex12.js
+++ ex12.js
@@ -4,7 +4,7 @@ var age= parseInt(prompt("Enter age"));
 if (age>125){
     alert("NOT A REAL AGE")
 }
-if (age > 0){
+else if (age >= 0){
     alert("REAL AGE")
 }
 else {
